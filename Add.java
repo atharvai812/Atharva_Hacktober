daf3d6a256957eb55f92d9cda2ea11de3fcb6b21
Sukhada Khade <130278445+sukhadak11@users.noreply.github.com>
2023-10-21 00:30:50 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 4;
+        int c = 3;
+        int Add = a+b+c;
+        System.out.println(Add);
+    }
+    
+}
